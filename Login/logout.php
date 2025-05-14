d5ae0d1c6447351a3e5ac4c38f00969065525c6c
Nito <62213492+Nitinho@users.noreply.github.com>
2025-05-14 15:51:50 +0100

    aaa

--- Login/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-session_unset();  // Remove todas as variáveis de sessão
-session_destroy(); // Destroi a sessão
-header("Location: login.php"); // Redireciona para a página de login
-exit();
-?>
